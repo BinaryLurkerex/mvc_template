80e227f7c2ad55183dbf7f5d4cf1e5ee42910237
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-06-05 00:42:59 +0300

    feat: add DI service

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:mvc_template/core/injector/services.dart';
+import 'package:mvc_template/src/app.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+  if (kDebugMode) {
+    Services.injectDev();
+  } else {
+    Services.injectProd();
   }
+
+  runApp(const App());
 }
