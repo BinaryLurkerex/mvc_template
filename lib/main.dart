bae016c4910d3df02ecaa28d17d570d2745645c4
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-06-05 02:34:42 +0300

    fix: add ensure init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:mvc_template/src/app_builder.dart';
 import 'package:mvc_template/src/view/injector/services.dart';
-import 'package:mvc_template/src/app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final GetIt getIt = await Services.inject();
   await getIt.allReady();
 
-  runApp(const App());
+  runApp(const AppBuilder());
 }
