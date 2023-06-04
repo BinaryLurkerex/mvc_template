9dbccfe0f32ca84ff6bd0a25b883e8c7c589b63d
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-06-05 02:10:44 +0300

    refactor: delete redundant method

---
+++ lib/src/view/injector/services.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:get_it/get_it.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:injectable/injectable.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'services.config.dart';
+
+@InjectableInit()
+class Services {
+  static final getIt = GetIt.instance;
+
+  static FutureOr<GetIt> inject() async {
+    HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory(),
+    );
+
+    return getIt.init(environment: Environment.prod);
+  }
+}
